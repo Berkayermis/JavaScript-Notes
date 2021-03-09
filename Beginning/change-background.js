a43e4d7b80a95b332629693b3f89ef6f972473f0
Berkay <67913214+Berkayermis@users.noreply.github.com>
2021-03-09 23:55:07 +0300

    Add files via upload

--- Beginning/change-background.js
+++ Beginning/change-background.js
@@ -1,4 +1,17 @@
-function myFunction(){
-    document.getElementById('demo').innerHTML = 'Default color:white';
-}
+
+// const button = document.querySelector('button');
+// const body = document.querySelector('body');
+// const colors = ['red','green','blue','grey','yellow','purple'];
+
+// body.background = white;
+// button.addEventListener('click', changeBg);
+
+// function changeBg(){
+//     const colorIndex = parseInt(Math.random()*colors.length);
+//     body.style.background = colors[colorIndex];
+// }
+
+
+
+
 
