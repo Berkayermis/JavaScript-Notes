194ef1dcd30e68ef5f9db82074d3b2ca615847b7
Berkay <67913214+Berkayermis@users.noreply.github.com>
2021-03-14 02:59:30 +0300

    Message application

---
+++ Beginning/Message-App/main.js
@@ -0,0 +1,30 @@
+
+// //Select the input element
+// const form = document.querySelector('#message-form')
+// //Set up Submit Button
+// form.addEventListener('submit', function(e){
+
+//     const msg = document.querySelector('#message')
+//     console.lo
+//     document.getElementById('demo').innerHTML = msg
+
+// })
+
+function myFunc(){
+    const msg = document.querySelector('#message')
+    document.getElementById('demo').innerHTML = msg.value
+
+}
+    
+
+
+
+
+
+
+
+// function myFunction(){
+//     const quoteIndex= parseInt(Math.random()*quotes.length)
+//     document.getElementById('demo').textContent = message
+// }
+
